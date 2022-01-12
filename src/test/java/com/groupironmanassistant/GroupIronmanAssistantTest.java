ede71ffb04ee0d09bb61803bb083083ddd0397cd
Tom Cadden <tcadden96@gmail.com>
2022-01-11 21:51:39 -0500

    Initial Plugin Setup
    Followed the basic setup instructions for a new plugin

--- src/test/java/com/groupironmanassistant/GroupIronmanAssistantTest.java
+++ src/test/java/com/groupironmanassistant/GroupIronmanAssistantTest.java
@@ -1,13 +1,13 @@
-package com.example;
+package com.groupironmanassistant;
 
 import net.runelite.client.RuneLite;
 import net.runelite.client.externalplugins.ExternalPluginManager;
 
-public class ExamplePluginTest
+public class GroupIronmanAssistantTest
 {
 	public static void main(String[] args) throws Exception
 	{
-		ExternalPluginManager.loadBuiltin(ExamplePlugin.class);
+		ExternalPluginManager.loadBuiltin(GroupIronmanAssistantPlugin.class);
 		RuneLite.main(args);
 	}
 }
